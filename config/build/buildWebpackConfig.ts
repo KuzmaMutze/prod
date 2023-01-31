dee40fbb1538236abacfbdc80e99563923f4337a
Matvey Fedorov <r3dw0r13@yandex.ru>
2023-02-01 03:13:37 +0400

    chore: dev server webpack

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,20 +3,24 @@ import webpack from 'webpack';
 import { buildLoader } from './buildLoader';
 import { buildPlugins } from './buildPlugins';
 import { buildResolver } from './buildResolver';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { mode, paths, isDev } = options;
     return {
-        mode: options.mode,
-        entry: options.paths.entry,
+        mode: mode,
+        entry: paths.entry,
         output: {
             filename: '[name].[contenthash].js',
-            path: options.paths.build,
+            path: paths.build,
             clean: true,
         },
-        plugins: buildPlugins(options.paths.html),
+        plugins: buildPlugins(paths.html),
         module: {
             rules: buildLoader(),
         },
         resolve: buildResolver(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     };
 }
