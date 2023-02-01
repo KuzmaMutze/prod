f0fabb0974e3eb2ae50117da1630c74d643d20c7
Matvey Fedorov <r3dw0r13@yandex.ru>
2023-02-01 23:10:47 +0400

    chore: React router dom, bundleб layz

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-import { Counter } from './components/Counter';
+import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.laze';
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
 import './index.scss';
+
 export function App() {
-    return <Counter />;
+    return (
+        <div>
+            <Link to="/">Main</Link>
+            <Link to="/about">About</Link>
+
+            <Suspense fallback={<div>Loading</div>}>
+                <Routes>
+                    <Route path="/about" element={<AboutPageLazy />} />
+                    <Route path="/" element={<MainPageLazy />} />
+                </Routes>
+            </Suspense>
+        </div>
+    );
 }
