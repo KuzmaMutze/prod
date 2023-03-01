a8f96d3fe6fe5f8fe4d9b1e6a0c2d56e6d999788
Matvey Fedorov <r3dw0r13@yandex.ru>
2023-03-01 22:26:26 +0400

    fix navbar

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,17 @@
+import { classNames } from 'shared/lib';
+import { AppLink, ColorAppLink } from 'shared/ui';
+import cls from './Navbar.module.scss';
+
+interface NavbarProps {
+  className?: string;
+}
+export const Navbar: React.FC<NavbarProps> = ({ className }) => (
+  <div className={classNames(cls.navbar, {}, [className])}>
+    <div className={cls.links}>
+      <AppLink color={ColorAppLink.SECONDARY} className={cls.mainLink} to="/">
+        {/* Main */}
+      </AppLink>
+      <AppLink to="/about">{/* About */}</AppLink>
+    </div>
+  </div>
+);
